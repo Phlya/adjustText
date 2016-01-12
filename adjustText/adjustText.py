0b0de9b89687caeda9b6f74349d075ed58ef5f3a
Ilya Flyamer <flyamer@gmail.com>
2016-01-12 10:55:29 +0000

    Bugfix to have renderer

--- adjustText/adjustText.py
+++ adjustText/adjustText.py
@@ -2,9 +2,13 @@ from matplotlib import pyplot as plt
 from matplotlib.transforms import Bbox
 import numpy as np
 
-def repel_text_annotations(texts, ax=None, expand=(1.2, 1.2), precision=0.1, prefer_move='xy', only_use_max_min=False):
+def repel_text_annotations(texts, renderer=None, ax=None, expand=(1.2, 1.2), precision=0.1, prefer_move='xy', only_use_max_min=False):
     if ax is None:
         ax = plt.gca()
+    if renderer is None:
+        r = ax.get_figure().canvas.get_renderer()
+    else:
+        r = renderer
     bboxes = [i.get_window_extent(r).expanded(*expand).transformed(plt.gca().transData.inverted()) for i in texts]
 
     overlaps_x = np.zeros((len(bboxes), len(bboxes)))
@@ -70,11 +74,15 @@ def get_midpoint(bbox):
     cy = (bbox.y0+bbox.y1)/2
     return cx, cy
 
-def repel_text_annotations_from_points(x, y, texts, ax=None, prefer_move='xy', expand=(1.2, 1.2),
+def repel_text_annotations_from_points(x, y, texts, renderer=None, ax=None, prefer_move='y', expand=(1.2, 1.2),
                                        precision=0.1, only_use_max_min=False):
     assert len(x) == len(y)
     if ax is None:
         ax = plt.gca()
+    if renderer is None:
+        r = ax.get_figure().canvas.get_renderer()
+    else:
+        r = renderer
     bboxes = [i.get_window_extent(r).expanded(*expand).transformed(plt.gca().transData.inverted()) for i in texts]
     move_x = np.zeros((len(bboxes), len(x)))
     move_y = np.zeros((len(bboxes), len(x)))
@@ -151,7 +159,13 @@ def repel_text_annotations_from_points(x, y, texts, ax=None, prefer_move='xy', e
         text.set_position((newx, newy))
     return texts, q
 
-def pull_text_annotations_to_respective_points(x, y, texts, fraction=0.1, expand=(1.2, 1.2)):
+def pull_text_annotations_to_respective_points(x, y, texts, renderer=None, ax=None, fraction=0.1, expand=(1.2, 1.2)):
+    if ax is None:
+        ax = plt.gca()
+    if renderer is None:
+        r = ax.get_figure().canvas.get_renderer()
+    else:
+        r = renderer
     bboxes = [i.get_window_extent(r).expanded(*expand).transformed(plt.gca().transData.inverted()) for i in texts]
     delta_x = []
     delta_y = []
@@ -176,25 +190,28 @@ def pull_text_annotations_to_respective_points(x, y, texts, fraction=0.1, expand
             texts[i].set_position((newx, newy))
     return texts
 
-def adjust_text(x, y, texts, expand_text = (1.2, 1.2), expand_points=(1.3, 1.3), prefer_move = 'y',
-                                        lim=100, precision=0.1, pullback_fraction=0.2, only_use_max_min=False,
+def adjust_text(x, y, texts, ax=None, expand_text = (1.2, 1.2), expand_points=(1.3, 1.3), prefer_move = 'y',
+                                        lim=100, precision=0.1, pullback_fraction=0.0, only_use_max_min=False,
                                         text_from_text=True, text_from_points=True, save_steps=False, save_prefix='',
                                         save_format='png',
                                         *args, **kwargs):
     #history1 = [np.inf]*5
     #history2 = [np.inf]*5
+    if ax is None:
+        ax = plt.gca()
+	r = ax.get_figure().canvas.get_renderer()
     if save_steps:
         plt.savefig(save_prefix+'0.'+save_format, format=save_format)
     for i in range(lim):
         q1, q2 = np.inf, np.inf
         if text_from_text:
-            texts, q1 = repel_text_annotations(texts, expand=expand_text, precision=precision,
+            texts, q1 = repel_text_annotations(texts, renderer=r, ax=ax, expand=expand_text, precision=precision,
                                                prefer_move=prefer_move, only_use_max_min=only_use_max_min)
         if text_from_points:
-            texts, q2 = repel_text_annotations_from_points(x, y, texts, expand=expand_points, prefer_move=prefer_move,
+            texts, q2 = repel_text_annotations_from_points(x, y, texts, ax=ax, renderer=r, expand=expand_points, prefer_move=prefer_move,
                                                            precision=precision, only_use_max_min=only_use_max_min)
         if pullback_fraction:
-            tests = pull_text_annotations_to_respective_points(x, y, texts, fraction=pullback_fraction, expand=expand_points)
+            tests = pull_text_annotations_to_respective_points(x, y, texts, renderer=r, ax=ax, fraction=pullback_fraction, expand=expand_points)
         plt.savefig(save_prefix+str(i+1)+'.'+save_format, format=save_format)
         q = np.array([q1, q2])[np.array([q1, q2])<np.inf]
         if i>=5 and np.all(q <= precision):# or (q1 >= max(history1) and q2 >= max(history2)):
@@ -207,7 +224,7 @@ def adjust_text(x, y, texts, expand_text = (1.2, 1.2), expand_points=(1.3, 1.3),
         #    history2.append(round(q2, 3))
     #print i, q1, q2
     for j, text in enumerate(texts):
-        plt.annotate(text.get_text(), xy = (x[j], y[j]), xytext=text.get_position(), *args, **kwargs)
+        ax.annotate(text.get_text(), xy = (x[j], y[j]), xytext=text.get_position(), *args, **kwargs)
         texts[j].set_visible(False)
     plt.savefig(save_prefix+str(i+1)+'.'+save_format, format=save_format)
     #history1[-1], history2[-1]
