a68a7141b74542d0cd0a0e197bd24f83b4d836c0
Ilya Flyamer <flyamer@gmail.com>
2016-02-04 23:29:45 +0000

    Timeseries example

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='adjustText',
-      version='0.5',
+      version='0.5.1',
       description='Iteratively adjust text position in matplotlib plots to minimize overlaps',
       author='Ilya Flyamer',
       author_email='flyamer@gmail.com',
