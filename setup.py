c8064fcd695465ec0f19bef8758f055908ccd6b3
Ilya Flyamer <flyamer@gmail.com>
2018-04-29 17:09:17 +0100

    Update setup.py
    Version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='adjustText',
-      version='0.6.2',
+      version='0.6.3',
       description='Iteratively adjust text position in matplotlib plots to minimize overlaps',
       author='Ilya Flyamer',
       author_email='flyamer@gmail.com',
